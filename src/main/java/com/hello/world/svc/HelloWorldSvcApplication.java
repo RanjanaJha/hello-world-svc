89f030346a6ba4ec3d9c9bc22c04462958f709d0
Jha <ranjana.jha@siemens.com>
2024-04-25 13:56:36 +0530

    initial commit

---
+++ src/main/java/com/hello/world/svc/HelloWorldSvcApplication.java
@@ -0,0 +1,13 @@
+package com.hello.world.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hello/world/svc/HelloWorldSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.world.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
